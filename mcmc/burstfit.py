[burstfit.py] fixed up plot_compare(). Removed x ticks, scaled by units

mcmc/burstfit.py
@@ -363,16 +363,25 @@ class BurstFit:
             burst property being compared
         """
         dx = 0.04  # horizontal offset of plot points
-        fig, ax = plt.subplots()
-        x = np.arange(1, self.n_epochs + 1)
+        yscale = {'dt': 1.0, 'fluence': 1e-6, 'peak': 1e-8, 'fper': 1e-9}.get(bprop)
+        y_units = {'dt': 'hr', 'fluence': '10^-6 erg/cm^2',
+                   'peak': '10^-8 erg/cm^2/s', 'fper': '10^-9 erg/cm^2/s'}.get(bprop)
 
-        ax.errorbar(x=x - dx, y=model, yerr=u_model, ls='none', marker='o',
+        fig, ax = plt.subplots(figsize=(6, 4))
+        ax.tick_params(axis='x',           # changes apply to the x-axis
+                       which='both',       # both major and minor ticks are affected
+                       bottom=False,       # ticks along the bottom edge are off
+                       top=False,          # ticks along the top edge are off
+                       labelbottom=False)  # labels along the bottom edge are off
+
+        x = np.arange(1, self.n_epochs + 1)
+        ax.errorbar(x=x - dx, y=model/yscale, yerr=u_model/yscale, ls='none', marker='o',
                     capsize=3, color='r', label='Model')
-        ax.errorbar(x=x + dx, y=obs, yerr=u_obs, ls='none',
+        ax.errorbar(x=x + dx, y=obs/yscale, yerr=u_obs/yscale, ls='none',
                     marker='o', capsize=3, color='b', label='Observed')
 
-        ax.set_ylabel(f'{bprop}')
-        ax.set_xlabel('epoch number')
+        ax.set_ylabel(f'{bprop} ({y_units})')
+        # ax.set_xlabel('epoch number')
         ax.set_title(bprop)
         ax.legend()
         plt.tight_layout()