14a1d7cdee5ebe1381f5b2af2cc6d88359664f17
Zac <zac.johnston@monash.edu>
2018-05-14 15:53:10 +1000

    [obs_tools.py] added get_source_string() and load_lightcurve()

--- grids/obs_tools.py
+++ grids/obs_tools.py
@@ -8,16 +8,30 @@ from ..grids import grid_strings
 GRIDS_PATH = os.environ['KEPLER_GRIDS']
 
 
-def load_sim_info(sim_batch, sim_name='sim'):
+def get_source_string(sim_batch, sim_name):
+    return f'{sim_name}{sim_batch}'
+
+
+def load_info(sim_batch, sim_name='sim'):
     """Load simulated data info
 
     Returns: pandas.DataFrame object"""
-    source = f'{sim_name}{sim_batch}'
+    source = get_source_string(sim_batch, sim_name)
     columns = ['series', 'number', 'dt', 'u_dt', 'fper', 'u_fper', 'bol']
 
     filename = f'{sim_name}_info.csv'
     path = grid_strings.get_obs_data_path(source)
     filepath = os.path.join(path, filename)
 
-    table = pd.read_csv(filepath, delim_whitespace=True, skiprows=16, names=columns)
-    return table
+    return pd.read_csv(filepath, delim_whitespace=True, skiprows=16, names=columns)
+
+
+def load_lightcurve(epoch, series, sim_batch, sim_name='sim'):
+    source = get_source_string(sim_batch, sim_name)
+    columns = ['time', 'time_step', 'flux', 'u_flux']
+
+    filename = f'{sim_name}{series}_{epoch}.csv'
+    path = grid_strings.get_obs_data_path(source)
+    filepath = os.path.join(path, 'lightcurves', filename)
+
+    return pd.read_csv(filepath, skiprows=16, names=columns)
