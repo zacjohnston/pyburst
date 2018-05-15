30089f403df4f4d4d81eebda681f8c2ab2909b04
Zac <zac.johnston@monash.edu>
2018-05-15 18:03:33 +1000

    [mcmc_jobs.py] renamed mcmc_job_submission.py to mcmc_jobs.py

--- mcmc/__init__.py
+++ mcmc/__init__.py
@@ -1,13 +1,13 @@
 from . import burstfit
 from . import mcmc
-from . import mcmc_job_submission
+from . import mcmc_jobs
 from . import mcmc_plotting
 from . import mcmc_tools
 from . import mcmc_versions
 
 __all__ = ['burstfit',
            'mcmc',
-           'mcmc_job_submission',
+           'mcmc_jobs.py',
            'mcmc_plotting',
            'mcmc_tools',
            'mcmc_versions',
