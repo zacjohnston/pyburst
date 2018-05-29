e28a8c9b15f11080d037dd258c0c3e2a30d05b35
Zac Johnston <zac.johnston@monash.edu>
2018-05-29 15:49:44 -0400

    [temp.py] added extract_dump()

--- misc/temp.py
+++ misc/temp.py
@@ -1,11 +1,12 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import os
 
 import kepdump
 
 
-def plot_saxj(x_units='time', dumptimes=True):
+def plot_saxj(x_units='time', dumptimes=True, cycles=None):
     """Plotting SAXJ1808 model, to explore dumpfiles
     to try and get temperature profiles"""
     filepath = '/home/zacpetej/archive/kepler/grid_94/xrb2/preload2.txt'
@@ -24,7 +25,11 @@ def plot_saxj(x_units='time', dumptimes=True):
         if x_units == 'time':
             ax.plot(lc[dumps, 0]/tscale, lc[dumps, 1], marker='o', ls='none')
         else:
-            ax.plot(dumps, lc[dumps, 1], marker='o', ls='none')
+            if x_units == 'time':
+                ax.plot(dumps, lc[dumps, 1], marker='o', ls='none')
+
+    if cycles is not None:
+        ax.plot(lc[cycles, 0], lc[cycles, 1], marker='o', ls='none')
 
     plt.show(block=False)
 
@@ -38,8 +43,30 @@ def load_dump(cycle, run=2, basename='xrb'):
     return kepdump.load(filepath)
 
 
+def extract_dump(cycle, run=2, basename='xrb'):
+    """Extracts key quantities of dump profile and saves as table
+    """
+    table = pd.DataFrame()
+
+    dump = load_dump(cycle, run=run, basename=basename)
+    table['y'] = dump.y[1:-2]
+    table['rho'] = dump.dn[1:-2]
+    table['T'] = dump.tn[1:-2]
+    table['P'] = dump.pn[1:-2]
+    table['R'] = dump.rn[1:-2]
+
+    return table
+
+
+def extract_cycles(cycles):
+    """Iterates over dump cycles and saves each as a table
+    """
+    pass
+
+
 def plot_temp(cycle, run=2, basename='xrb'):
-    """Plot temperature profile at given cycle (timestep)"""
+    """Plot temperature profile at given cycle (timestep)
+    """
     dump = load_dump(cycle, run=run, basename=basename)
     y0 = dump.y[1]  # column depth at inner zone
 
