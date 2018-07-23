c20a0f70c387a63c10bdeed1b2f99f8ba6c9525a
Zac <zac.johnston@monash.edu>
2018-07-23 16:38:14 +1000

    [grid_analyser.py] commented out conflicting method get_nruns()

--- grids/grid_analyser.py
+++ grids/grid_analyser.py
@@ -275,12 +275,12 @@ class Kgrid:
         dt = (np.exp(1) ** y0) * (mdot ** m)
         return dt
 
-    def get_nruns(self, params):
-        """Returns the total number of runs in some slice of parameter space
-        """
-        # TOD
-        total = 0
-        pass
+    # def get_nruns_params(self, params):
+    #     """Returns the total number of runs in some slice of parameter space
+    #     """
+    #     # TOD
+    #     total = 0
+    #     pass
 
     def plot_mean_lc(self, batch, run, show=True):
         """Plots mean lightcurve for given batch model
