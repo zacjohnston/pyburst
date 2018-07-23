20df9d782618260dbc637b99f5a774031268f2e6
Zac <zac.johnston@monash.edu>
2018-07-23 17:54:33 +1000

    [best_lc.py] plot all 3 bursts. Added fill_between

--- misc/best_lc.py
+++ misc/best_lc.py
@@ -21,23 +21,32 @@ class Best:
         self.redshift = self.best_params[red_idx]
         self.f_b = self.best_params[fb_idx]
 
-    def plot(self, burst):
+    def plot(self):
         tshifts = [8, 8, 8]
-        fig, ax = plt.subplots()
+        n_bursts = len(tshifts)
+        fig, ax = plt.subplots(n_bursts, 1, sharex=True, figsize=(10, 12))
 
-        obs_burst = self.bfit.obs[burst]
-        obs_x = np.array(obs_burst.time)
-        obs_y = np.array(obs_burst.flux)
-        obs_y_u = np.array(obs_burst.flux_err)
+        for burst in range(n_bursts):
+            obs_burst = self.bfit.obs[burst]
+            obs_x = np.array(obs_burst.time)
+            obs_y = np.array(obs_burst.flux)
+            obs_y_u = np.array(obs_burst.flux_err)
 
-        model = self.grid.mean_lc[1][burst+1]
-        lum_to_flux = self.redshift * 4 * np.pi * self.f_b * 1e45
+            model = self.grid.mean_lc[1][burst+1]
+            lum_to_flux = self.redshift * 4 * np.pi * self.f_b * 1e45
 
-        m_x = (model[:, 0] * self.redshift) + tshifts[burst]
-        m_y = model[:, 1] / lum_to_flux
-        m_y_u = model[:, 2] / lum_to_flux
+            m_x = (model[:, 0] * self.redshift) + tshifts[burst]
+            m_y = model[:, 1] / lum_to_flux
+            m_y_u = model[:, 2] / lum_to_flux
+            m_y_upper = m_y + m_y_u
+            m_y_lower = m_y - m_y_u
 
-        ax.errorbar(obs_x, obs_y, yerr=obs_y_u, ls='none', capsize=3)
-        ax.plot(m_x, m_y)
+            ax[burst].fill_between(m_x, m_y_lower, m_y_upper, color='0.7')
+            ax[burst].plot(m_x, m_y, color='black')
+            ax[burst].errorbar(obs_x, obs_y, yerr=obs_y_u, ls='none', capsize=3, color='C1')
 
+        ax[-1].set_xlabel('Time (s)', fontsize=20)
+        ax[1].set_ylabel(r'Flux (erg cm$^{-2}$ s$^{-1}$)', fontsize=20)
+        ax[-1].set_xlim([-10, 200])
+        plt.tight_layout()
         plt.show(block=False)
