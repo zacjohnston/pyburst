d840df23b20082200461bb064f2fcf916678e733
Zac <zac.johnston@monash.edu>
2018-08-06 20:43:34 +1000

    [burst_pipeline.py] modified to extract_batches()

--- burst_analyser/burst_pipeline.py
+++ burst_analyser/burst_pipeline.py
@@ -54,25 +54,31 @@ def run_analysis(batches, source, copy_params=True, reload=True, multithread=Tru
         burst_tools.combine_extracts(np.arange(1, last_batch + 1), source)
 
 
-def multithread_extract(batches, source, plot_model=True, plot_convergence=True,
-                        plot_linregress=True):
-    args = []
-    for batch in batches:
-        args.append([batch, source, plot_model, plot_convergence, plot_linregress])
+# def multithread_extract(batches, source, plot_model=True, plot_convergence=True,
+#                         plot_linregress=True):
+#     for batch in batches:
+#         args = []
+#         n_runs = grid_tools.get_nruns(batch, source)
+#         runs = np.arange(1, n_runs+1)
+#
+#         for run in runs:
+#             # args.append((run, batch, source, basename, True))
+#             args.append([batch, source, plot_model, plot_convergence, plot_linregress])
+#         # with mp.Pool(processes=8) as pool:
+#         #     pool.starmap(load, args)
+#
+#     t0 = time.time()
+#     with mp.Pool(processes=8) as pool:
+#         pool.starmap(extract_batches, args)
+#     t1 = time.time()
+#     dt = t1 - t0
+#     print(f'Time taken: {dt:.1f} s ({dt/60:.2f} min)')
 
-    t0 = time.time()
-    with mp.Pool(processes=8) as pool:
-        pool.starmap(extract_bursts, args)
-    t1 = time.time()
-    dt = t1 - t0
-    print(f'Time taken: {dt:.1f} s ({dt/60:.2f} min)')
 
-
-def extract_bursts(batches, source, plot_model=True, plot_convergence=True,
-                   plot_linregress=True):
-    source_path = grid_strings.get_source_path(source)
-    batches = grid_tools.expand_batches(batches, source)
-    bool_map = {True: 'Y', False: 'N'}
+def extract_batches(batches, source, save_plots=True, multithread=True):
+    """Do burst analysis on arbitrary number of batches"""
+    analysis_path = grid_strings.get_source_subdir(source, 'burst_analysis')
+    batches = grid_tools.ensure_np_list(batches)
 
     flags = ('converged',)
     b_ints = ('batch', 'run', 'num', 'discard')
@@ -81,66 +87,37 @@ def extract_bursts(batches, source, plot_model=True, plot_convergence=True,
                  'fluence', 'u_fluence', 'length', 'u_length', 'peak', 'u_peak']
 
     for batch in batches:
+        print_title(f'Batch {batch}')
         batch_str = f'{source}_{batch}'
-        analysis_path = os.path.join(source_path, 'burst_analysis', batch_str)
+        batch_path = os.path.join(analysis_path, batch_str)
         grid_tools.try_mkdir(analysis_path, skip=True)
 
         filename = f'burst_analysis_{batch_str}.txt'
         filepath = os.path.join(analysis_path, filename)
 
-        data = {}
-        for bp in bprops:
-            u_bp = f'u_{bp}'
-            data[bp] = []
-            data[u_bp] = []
-
-        data['rate'] = []
-        data['u_rate'] = []
-
-        for b in (b_ints + flags):
-            data[b] = []
-
         n_runs = grid_tools.get_nruns(batch, source)
         for run in range(1, n_runs + 1):
-            sys.stdout.write(f'\r{source}_{batch} xrb{run:02}')
-            burstfit = burst_analyser.BurstRun(run, batch, source,
-                                               analyse=True)
-            data['batch'] += [batch]
-            data['run'] += [run]
-            data['num'] += [burstfit.n_bursts]
-            data['discard'] += [burstfit.discard]
-            data['converged'] += [bool_map[burstfit.flags['converged']]]
+            extract_runs(run, batch, source, save_plots=save_plots)
 
-            for bp in bprops:
-                u_bp = f'u_{bp}'
-                data[bp] += [burstfit.summary[f'mean_{bp}']]
-                data[u_bp] += [burstfit.summary[f'std_{bp}']]
-
-            if not burstfit.flags['too_few_bursts']:
-                if plot_model:
-                    burstfit.plot(short_wait=True, outliers=True, display=False, save=True)
-                if plot_convergence:
-                    burstfit.plot_convergence(display=False, save=True)
-
-                if plot_linregress and not burstfit.flags['regress_too_few_bursts']:
-                    burstfit.plot_linregress(display=False, save=True)
-
-        table = pd.DataFrame(data)
-        table = table[col_order]
-        table_str = table.to_string(index=False, justify='left', col_space=12,
-                                    formatters={'discard': '{:.0f}'.format})
-
-        with open(filepath, 'w') as f:
-            f.write(table_str)
+        # table = table[col_order]
+        # table_str = table.to_string(index=False, justify='left',
+        #                             formatters={'discard': '{:.0f}'.format})
+        # with open(filepath, 'w') as f:
+        #     f.write(table_str)
 
 
-def extract_runs(runs, batch, source):
+def extract_runs(runs, batch, source, save_plots=True):
     """Do burst analysis on run(s) from a single batch and save results
     """
     runs = grid_tools.ensure_np_list(runs)
-
     for run in runs:
         print_title(f'Run {run}')
         model = burst_analyser.BurstRun(run, batch, source, analyse=True)
         model.save_burst_table()
         model.save_summary_table()
+
+        if save_plots:
+            model.plot(display=False, save=True)
+            model.plot_convergence(display=False, save=True)
+            model.plot_linregress(display=False, save=True)
+
