0498ae41a8ceb74b17f074bcc8731742bd861cdc
Zac <zac.johnston@monash.edu>
2018-08-28 18:14:13 +1000

    [misc/resolution.py] defined y_labels, units etc., added set_axes()

--- misc/resolution.py
+++ misc/resolution.py
@@ -5,6 +5,25 @@ from pygrids.grids import grid_analyser
 
 # resolution tests
 
+y_factors = {'dt': 3600,
+             'fluence': 1e39,
+             'peak': 1e38,
+             }
+
+y_labels = {'dt': '$\Delta t$',
+            'rate': 'Burst rate',
+            'fluence': '$E_b$',
+            'peak': '$L_{peak}$',
+            'length': 'Burst length',
+            }
+
+y_units = {'dt': 'hr',
+           'rate': 'day$^{-1}$',
+           'fluence': '$10^39$ erg',
+           'peak': '$10^38$ erg s$^{-1}$',
+           'length': 's',
+           }
+
 def plot(params, res_param, sources, bprop='rate'):
     """Plot burst properties for given resolution parameter
 
@@ -13,23 +32,26 @@ def plot(params, res_param, sources, bprop='rate'):
     params : {}
     res_param : str
         resolution parameter to plot on x-axis. One of [accmass, accrate]
-    source : [str]
+    sources: [str]
         list of source(s) to get models from
+    bprop : str
     """
     check_params(params)
     u_bprop = f'u_{bprop}'
     grids, sub_summ, sub_params = get_multi_subgrids(params=params, sources=sources)
     fig, ax = plt.subplots()
 
+    y_label = f'{y_labels[bprop]} ({y_units[bprop]})'
+    y_factor = y_factors.get(bprop, 1)
+
     for source in sources:
-        ax.errorbar(x=sub_params[source][res_param], y=sub_summ[source][bprop],
-                    yerr=sub_summ[source][u_bprop], ls='none', marker='o',
-                    capsize=3, color='C0')
+        ax.errorbar(x=sub_params[source][res_param],
+                    y=sub_summ[source][bprop] / y_factor,
+                    yerr=sub_summ[source][u_bprop] / y_factor,
+                    ls='none', marker='o', capsize=3, color='C0')
 
-    ax.set_xscale('log')
+    set_axes(ax, xlabel=res_param, ylabel=y_label, xscale='log')
     plt.show(block=False)
-
-    return sub_params, sub_summ
 
 
 def get_multi_subgrids(params, sources):
@@ -45,6 +67,14 @@ def get_multi_subgrids(params, sources):
         sub_summ[source] = grids[source].get_summ(params=params)
 
     return grids, sub_summ, sub_params
+
+
+def set_axes(ax, title='', xlabel='', ylabel='', yscale='linear', xscale='linear', fontsize=14):
+    ax.set_title(title, fontsize=fontsize)
+    ax.set_xlabel(xlabel, fontsize=fontsize)
+    ax.set_ylabel(ylabel, fontsize=fontsize)
+    ax.set_xscale(xscale)
+    ax.set_yscale(yscale)
 
 
 def check_params(params, must_specify=('x', 'z', 'accrate', 'mass')):
