ed6617e61bb20cdf8f6fdff01775481036942bba
Zac <zac.johnston@monash.edu>
2018-10-19 16:21:56 +1100

    [sample.py] added get_all_tshifts(), get_tshift(), and chi_squared()

--- mcmc/sample.py
+++ mcmc/sample.py
@@ -67,6 +67,56 @@ class Ksample:
                                                                   bounds_error=False,
                                                                   fill_value=0)
 
+    def get_all_tshifts(self):
+        """Gets best t_shift for all bursts
+        """
+        t_shifts = np.full((self.n_epochs, len(self.runs)), np.nan)
+
+        for i in range(self.n_epochs):
+            for j, run in enumerate(self.runs):
+                t_shifts[i, j] = self.fit_tshift(run=run, epoch_i=i)
+
+        self.t_shifts = t_shifts
+
+    def fit_tshift(self, run, epoch_i, n_points=500):
+        """Finds LC tshift that minimises chi^2
+
+        Note: assumes epoch_i correspond to index of batches
+        """
+        # TODO: Bug when model LC shorter than obs LC
+        # batch = self.batches[epoch_i]
+        # obs = self.bfit.obs[epoch_i]
+        # model = self.shifted_lc[batch][run]
+        # min_tshift = (obs.time[-1].value + 0.5*obs.dt[-1].value
+        #               - model[-1, 0])
+        # max_tshift = (obs.time[0].value + 0.5*obs.dt[0].value
+        #               - model[0, 0])
+        min_tshift = -60
+        max_tshift = 60
+        t = np.linspace(min_tshift, max_tshift, n_points)
+        chi2 = np.zeros_like(t)
+
+        for i in range(n_points):
+            chi2[i] = self.chi_squared(t[i], epoch_i=epoch_i, run=run)
+
+        min_idx = np.argmin(chi2)
+        return t[min_idx]
+
+    def chi_squared(self, tshift, epoch_i, run):
+        """Returns chi^2 of model vs. observed lightcurves
+        """
+        obs_burst = self.bfit.obs[epoch_i]
+        obs_x = np.array(obs_burst.time + 0.5*obs_burst.dt)
+        obs_flux = np.array(obs_burst.flux)
+        obs_flux_err = np.array(obs_burst.flux_err)
+
+        batch = self.batches[epoch_i]
+        model = self.interp_lc[batch][run]
+        model_flux = model['flux'](obs_x - tshift)
+        model_flux_err = model['flux_err'](obs_x - tshift)
+
+        return np.sum((obs_flux - model_flux)**2 / np.sqrt(obs_flux_err**2 + model_flux_err**2))
+
 
 def plot_batch(source, batch, error=False):
     kgrid = grid_analyser.Kgrid(source=source, linregress_burst_rate=False,
