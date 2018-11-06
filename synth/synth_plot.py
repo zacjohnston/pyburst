43d71feb7c0407647cd1883601408b1c2bda7d82
Zac <zac.johnston@monash.edu>
2018-11-06 17:33:30 +1100

    [synth_plot.py] added plot_posteriors()

--- synth/synth_plot.py
+++ synth/synth_plot.py
@@ -3,9 +3,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 from pygrids.synth import synth
+from pygrids.mcmc import mcmc_plot, mcmc_versions
 
 
-def plot_posteriors():
+def plot_posteriors(chain, source, mc_version, discard, **kwargs):
     """Plots mcmc posteriors for synthetic data
     """
-    pass
+    mcv = mcmc_versions.McmcVersion(source, mc_version)
+
+    truth = synth.get_true_values(source, version=mcv.synth_version,
+                                  group=mcv.synth_group)
+    mcmc_plot.plot_posteriors(chain, source=source, version=mcv.synth_version,
+                              discard=discard, truth_values=truth, **kwargs)
