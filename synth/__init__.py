1fc7620124e90aebcdbe879d818043980bdf6632
Zac <zac.johnston@monash.edu>
2018-11-06 16:47:03 +1100

    [synth_plot.py] created synth_plot.py

--- synth/__init__.py
+++ synth/__init__.py
@@ -1,6 +1,9 @@
 from . import synth
 from . import synth_data
+from . import synth_plot
+
 __all__ = [
     'synth',
     'synth_data',
+    'synth_plot',
 ]
---
+++ synth/synth_plot.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from pygrids.synth import synth
+
+
+def plot_posteriors():
+    """Plots mcmc posteriors for synthetic data
+    """
+    pass
