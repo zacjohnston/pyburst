46fd75c898d3283c6f9520ebc0d15acabf829fd1
Zac <zac.johnston@monash.edu>
2018-11-12 18:24:07 +1100

    [plotting_tools.py] added units_scale()

--- plotting/plotting_tools.py
+++ plotting/plotting_tools.py
@@ -15,3 +15,15 @@ def set_axes(ax, title='', xlabel='', ylabel='', yscale='linear', xscale='linear
     ax.set_ylabel(ylabel, fontsize=fontsize)
     ax.set_xscale(xscale)
     ax.set_yscale(yscale)
+
+
+def units_scale(quantity):
+    """Returns typical scale factor for given quantity
+    """
+    scales = {
+        'rate': 1.0,
+        'dt': 1.0,
+        'fluence': 1e39,
+        'peak': 1e38,
+    }
+    return scales.get(quantity, 1.0)
