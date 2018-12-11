4367545bde6ffd4b489d786203e730ed5662871a
Zac <zac.johnston@monash.edu>
2018-12-11 13:04:34 +1100

    [accretion.py] added eddington_lum()

--- pyburst/physics/accretion.py
+++ pyburst/physics/accretion.py
@@ -1,2 +1,20 @@
 import numpy as np
+from astropy import units
+import astropy.constants as const
 
+def eddington_lum(mass, x):
+    """Returns the spherical Eddington luminosity for a given solar mass and composition
+       (erg / s)
+
+    Parameters
+    ----------
+    mass : flt
+        solar mass (M_sun)
+    x : flt
+        hydrogen composition (mass fraction)
+    """
+    l_edd = 4*np.pi * const.G * (mass*units.M_sun) * const.m_p * const.c / const.sigma_T
+    l_edd = l_edd.to(units.erg / units.s).value   # cgs units
+    l_edd = l_edd * 2 / (x + 1)  # correct for hydrogen/helium ratio
+
+    return l_edd
