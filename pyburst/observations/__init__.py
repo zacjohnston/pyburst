bce610aa93fdf16001dce2e5d41c3272206d0d35
Zac <zac.johnston@monash.edu>
2018-12-12 15:32:01 +1100

    [observations/] created new package for handling observed data

---
+++ pyburst/observations/__init__.py
@@ -0,0 +1,4 @@
+from . import obs_tools
+
+__all__ = ['obs_tools',
+           ]
---
+++ pyburst/observations/obs_tools.py
@@ -0,0 +1,11 @@
+import numpy as np
+import os
+
+
+# pyburst
+
+
+def load_summary():
+    """Loads summary of observed data
+    """
+    pass
