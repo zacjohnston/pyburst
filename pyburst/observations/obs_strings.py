cd1473b89985bf1de84cfc10fa2bde438bf759dd
Zac <zac.johnston@monash.edu>
2018-12-13 15:37:03 +1100

    [obs_strings.py] added summary_filepath()

--- pyburst/observations/obs_strings.py
+++ pyburst/observations/obs_strings.py
@@ -1,5 +1,9 @@
 import os
 
+PYBURST_PATH = os.environ['PYBURST']
+OBS_DATA_PATH = os.path.join(PYBURST_PATH, 'files', 'obs_data')
+
 
 def summary_filepath(source):
-    pass
+    filename = f'{source}.dat'
+    return os.path.join(OBS_DATA_PATH, source, filename)
