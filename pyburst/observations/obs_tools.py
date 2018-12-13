50224f745b1ed756e540ff45919461a87a590950
Zac <zac.johnston@monash.edu>
2018-12-13 16:19:14 +1100

    [obs_tools.py] added load_epoch_lightcurve()

--- pyburst/observations/obs_tools.py
+++ pyburst/observations/obs_tools.py
@@ -8,7 +8,6 @@ from . import obs_strings
 from pyburst.grids.grid_tools import write_pandas_table
 
 # TODO:
-#   - load summary
 #   - load epoch
 #   - add rate column
 #   - add length column
@@ -34,3 +33,21 @@ def save_summary(table, source):
     """
     filepath = obs_strings.summary_filepath(source)
     write_pandas_table(table, filepath=filepath)
+
+
+def load_epoch_lightcurve(epoch, source):
+    """Returns table of epoch lightcurve data
+
+    parameters
+    ----------
+    epoch : int
+    source : str
+    """
+    # Time [s], dt [s], average flux and error [10^-9 erg/cm^2/s], average
+    # blackbody temperature and error [keV], average blackbody normalisation
+    # and error [(km/d_10kpc)^2]
+    columns = ('time', 'dt', 'flux', 'u_flux', 'kt', 'u_kt', 'normal', 'u_normal', 'chi2')
+    filepath = obs_strings.epoch_filepath(epoch=epoch, source=source)
+    table = pd.read_csv(filepath, delim_whitespace=True, comment='#', header=None,
+                        names=columns)
+    return table
