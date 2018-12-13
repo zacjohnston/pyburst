854430e778a7cb532406e3d58241cb7291af8e3c
Zac <zac.johnston@monash.edu>
2018-12-13 15:48:21 +1100

    [obs_tools.py] added save_summary()

--- pyburst/observations/obs_tools.py
+++ pyburst/observations/obs_tools.py
@@ -5,6 +5,7 @@ import os
 
 # pyburst
 from . import obs_strings
+from pyburst.grids.grid_tools import write_pandas_table
 
 # TODO:
 #   - load summary
@@ -14,6 +15,22 @@ from . import obs_strings
 
 def load_summary(source):
     """Loads summary of observed data
+
+    parameters
+    ----------
+    source : str
     """
     filepath = obs_strings.summary_filepath(source)
     return pd.read_csv(filepath, delim_whitespace=True)
+
+
+def save_summary(table, source):
+    """Saves summary table to file
+
+    parameters
+    ----------
+    table : pandas.DataFrame
+    source : str
+    """
+    filepath = obs_strings.summary_filepath(source)
+    write_pandas_table(table, filepath=filepath)
