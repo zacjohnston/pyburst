571563aec58464bccd9f07b57b99e71764c1e6be
Zac <zac.johnston@monash.edu>
2019-01-23 16:00:02 +1100

    [gravity.py] reformatted equation in gr_corrections()

--- pyburst/physics/gravity.py
+++ pyburst/physics/gravity.py
@@ -136,11 +136,11 @@ def gr_corrections(r, m, phi=1.0, verbose=False):
     """
     zeta = get_zeta(r=r, m=m)
 
-    B = (9*zeta**2*phi**4 + np.sqrt(3)*phi**3 * np.sqrt(16 + 27*zeta**4 * phi**2))**(1/3)
-    A = (2/9)**(1/3)*(B**2/phi**2 - 2*(6)**(1/3))/(B*zeta**2)
-    xi = (zeta*phi/2)*(1 + np.sqrt(1-A) + np.sqrt(2+A+2/np.sqrt(1-A)))
+    b = (9*zeta**2*phi**4 + np.sqrt(3)*phi**3 * np.sqrt(16 + 27*zeta**4 * phi**2))**(1/3)
+    a = (2/9)**(1/3) * (b**2 / phi**2 - 2 * 6**(1/3)) / (b * zeta**2)
+    xi = (zeta * phi/2) * (1 + np.sqrt(1 - a) + np.sqrt(2 + a + 2 / np.sqrt(1 - a)))
 
-    redshift = xi**2/phi    #NOTE: unrelated to anisotropy xi factor
+    redshift = xi**2/phi    # NOTE: xi unrelated to anisotropy factor
 
     if verbose:
         print_title(f'Using R={r:.3f} and M={m}:')
