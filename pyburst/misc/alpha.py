e6befbebb946192e1809f5f91c9244d6d6c3182b
Zac <zac.johnston@monash.edu>
2019-01-23 17:09:48 +1100

    [alpha.py] added add_phi()

--- pyburst/misc/alpha.py
+++ pyburst/misc/alpha.py
@@ -18,10 +18,8 @@ def add_redshift_radius_gr(kgrid, m_ratio=1.0):
 
     kgrid : grid_analyser.Kgrid
         grid object containing model data
-    m_ratio : flt
+    m_ratio : flt (optional)
         mass ratio, M_gr / M_newton
-    radius : flt | array
-        newtonian radius of models
     """
     default_radius = 10
 
@@ -34,15 +32,23 @@ def add_redshift_radius_gr(kgrid, m_ratio=1.0):
 
     r_ratios, redshifts = gravity.gr_corrections(r=radii, m=masses, phi=m_ratio)
     kgrid.params['radius_gr'] = radii * r_ratios
+    kgrid.params['mass_gr'] = masses * m_ratio
     kgrid.params['redshift'] = redshifts
 
 
-def add_phi(kgrid, m_ratio=1.0, radius=None):
+def add_phi(kgrid):
     """Adds phi (gravitational potential) column to given Kgrid
+        Requires kgrid.params columns: radius_gr, redshift
+        If these don't exist, run add_phi()
 
     kgrid : grid_analyser.Kgrid
         grid object containing model data
     m_ratio : flt
         mass ratio, M_gr / M_newton
     """
+    if ('radius_gr' not in kgrid.params.columns) \
+            or ('redshift' not in kgrid.params.columns):
+        add_redshift_radius_gr(kgrid)
 
+    phi = gravity.get_potential_gr(redshift=kgrid.params.redshift)
+    kgrid.params['phi'] = phi
