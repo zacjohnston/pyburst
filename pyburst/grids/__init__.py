1728abbaa4c7dad6fdd6a15e72e8f032426a419e
Zac <zac.johnston@monash.edu>
2019-02-12 17:36:16 +1100

    [grids/__init__.py] added grid_versions

--- pyburst/grids/__init__.py
+++ pyburst/grids/__init__.py
@@ -3,10 +3,12 @@ from . import grid_plotting
 from . import grid_setup
 from . import grid_strings
 from . import grid_tools
+from . import grid_versions
 
 __all__ = ['grid_analyser',
            'grid_plotting',
            'grid_setup',
            'grid_strings',
            'grid_tools',
+           'grid_versions',
            ]
