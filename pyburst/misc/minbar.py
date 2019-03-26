e43f43a4b7c22e7d796e5b1e304ec059ebd35350
Zac <zac.johnston@monash.edu>
2019-03-26 18:20:47 +1100

    [minbar.py] added Minbar class, and get_source()

--- pyburst/misc/minbar.py
+++ pyburst/misc/minbar.py
@@ -2,6 +2,9 @@ import numpy as np
 import pandas as pd
 import os
 
+# pyburst
+from pyburst.grids import grid_tools
+
 """
 Uses minbar table from:
 https://burst.sci.monash.edu/wiki/uploads/MINBAR/minbar.txt 
@@ -14,5 +17,22 @@ filepath = os.path.join(pyburst_path, 'files', filename)
 def load_table():
     """Loads minbar table from URL
     """
-    table = pd.read_table(filepath, delim_whitespace=True, header=9)
-    return table
+    return pd.read_table(filepath, delim_whitespace=True, header=9)
+
+
+class Minbar:
+    """
+    Class for exploring the MINBAR catalogue
+    """
+    def __init__(self):
+        self.table = load_table()
+        self.sources = np.unique(self.table['name'])
+
+    def get_source(self, source):
+        """Returns subset of minbar table for given source
+        """
+        if source not in self.sources:
+            raise ValueError(f'source "{source}" not in minbar table. '
+                             'Look in self.sources for full list')
+
+        return grid_tools.reduce_table(self.table, params={'name': source})
