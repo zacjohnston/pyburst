a53aa7d77228ea7d3a267354b126c6c30d3ee92e
Zac <zac.johnston@monash.edu>
2019-04-17 19:26:23 +1000

    [scripts/push_grids.sh] updated for new directory structure

--- scripts/push_grids.sh
+++ scripts/push_grids.sh
@@ -2,7 +2,7 @@
 #=============================================
 # Pushes kepler models to remote monarch (assumes set of 3 batches)
 #=============================================
-if [ "$1" == "-h" ]; then
+if [[ "$1" == "-h" ]]; then
   echo "usage: `basename $0` [source] [batch0] [batch1]
 where:
     -source   the X-ray source being modelled (e.g. gs1826)
@@ -11,7 +11,7 @@ where:
     -cluster  cluster to push to [monarch, icer]"
   exit 0
 fi
-if [ $# -ne 4 ]; then
+if [[ $# -ne 4 ]]; then
   echo "Error: must supply 4 arguments (use option -h for help):
 1. source
 2. batch0
@@ -20,26 +20,32 @@ if [ $# -ne 4 ]; then
   exit 1
 fi
 #=============================================
-DIR=$KEPLER_MODELS
+DIR=${KEPLER_MODELS}
 source=${1}
 batch0=${2}
 batch1=${3}
 cluster=${4}
 
-if [ "${cluster}" == "monarch" ]; then
-  TARGET='m2:/home/zacpetej/id43/kepler/runs'
-elif [ "${cluster}" == "icer" ]; then
-  TARGET='icer:/mnt/home/f0003004/kepler/runs'
-elif [ "${cluster}" == "oz" ]; then
-  TARGET='oz:/fred/oz011/zac/kepler/runs'
+if [[ "${cluster}" == "monarch" ]]; then
+  SERVER='m2'
+  KPATH='/home/zacpetej/id43/kepler/runs'
+elif [[ "${cluster}" == "icer" ]]; then
+  SERVER='icer'
+  KPATH='/mnt/home/f0003004/kepler/runs'
+elif [[ "${cluster}" == "oz" ]]; then
+  SERVER='oz'
+  KPATH='/fred/oz011/zac/kepler/runs'
 else
   echo "Must choose one of (icer, monarch, oz)"
   exit 1
 fi
+TARGET=${SERVER}:${KPATH}/${source}
 
 for batch in $(seq ${batch0} ${batch1}); do
   batches="${batches} ${source}_${batch}"
 done
 
-cd ${DIR}
+cd ${DIR}/${source}
+
+ssh ${SERVER} mkdir -p ${TARGET}
 rsync -av ${batches} ${TARGET}
