6ed34d3e3f60db6f8c45e405632a1011fddf8bb9
Zac <zac.johnston@monash.edu>
2019-06-19 18:54:30 +1000

    [zcno_tools.py] split out plot options from fit_beta() and fit_gaussian() into plot_fit()

--- pyburst/mcmc/zcno_tools.py
+++ pyburst/mcmc/zcno_tools.py
@@ -5,6 +5,34 @@ from scipy.stats import norm, beta
 
 # TODO:
 
+def plot_fit(table, fit='beta', var='feh', bins=100, hisstype='step', display=True,
+             xlims=(-3.5, 1.0), func=None):
+    """Plots fit of distribution to given table
+
+    fit : str
+        one of 'gaussian' or 'beta'
+    """
+    if func is None:
+        if fit == 'gaussian':
+            loc, scale = fit_gaussian(table, xlims=xlims)
+            func = norm(loc=loc, scale=scale).pdf
+        elif fit == 'beta':
+            a, b, loc, scale = fit_beta(table, xlims=xlims)
+            func = beta(a, b, loc=loc, scale=scale).pdf
+
+    x = np.linspace(xlims[0], xlims[1], 200)
+    y = func(x)
+
+    fig, ax = plot_hist(table=table, var=var, bins=bins, histtype=hisstype)
+    ax.plot(x, y, label=fit)
+    ax.legend()
+
+    if display:
+        plt.show(block=False)
+
+    return fig, ax
+
+
 def plot_hist(table, var='feh', bins=100, histtype='step', display=True,
               values=None):
     """Plots histogram of the given table variable
@@ -25,7 +53,7 @@ def plot_hist(table, var='feh', bins=100, histtype='step', display=True,
     return fig, ax
 
 
-def fit_gaussian(table, plot=False, xlims=(-3, 3)):
+def fit_gaussian(table, xlims=(-3, 3)):
     """Returns Gaussian fit (mean, std) to a given [Fe/H] distribution
 
     table : pd.DataFrame
@@ -33,20 +61,15 @@ def fit_gaussian(table, plot=False, xlims=(-3, 3)):
     plot : bool
         show Gaussian fit against distribution histogram
     """
-    mean, std = norm.fit(table['feh'])
+    z_sort = np.sort(table['feh'])
+    i_0 = np.searchsorted(z_sort, xlims[0])
+    i_1 = np.searchsorted(z_sort, xlims[1])
 
-    if plot:
-        x = np.linspace(xlims[0], xlims[1], 200)
-        y = norm.pdf(x, mean, std)
-
-        fig, ax = plot_hist(table=table, var='feh')
-        ax.plot(x, y)
-        plt.show(block=False)
-
+    mean, std = norm.fit(table['feh'][i_0:i_1])
     return mean, std
 
 
-def fit_beta(table, plot=False, xlims=(-2, 0.5)):
+def fit_beta(table, xlims=(-2, 0.5)):
     """Returns fit of Beta Distribution to a given [Fe/H] table
     See: fit_gaussian()
     """
@@ -58,13 +81,4 @@ def fit_beta(table, plot=False, xlims=(-2, 0.5)):
     scale = xlims[1] - xlims[0]
 
     a, b, loc, scale = beta.fit(z_sort[i_0:i_1], floc=loc, fscale=scale)
-
-    if plot:
-        x = np.linspace(xlims[0], xlims[1], 100)
-        y = beta.pdf(x, a, b, loc=loc, scale=scale)
-
-        fig, ax = plot_hist(table=table, var='feh', values=z_sort)
-        ax.plot(x, y)
-        plt.show(block=False)
-
     return a, b, loc, scale
