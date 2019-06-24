e77a2725f0938e6d0bc616f47d800af2bd2bc858
Zac <zac.johnston@monash.edu>
2019-06-24 16:00:16 +1000

    [misc/transform.py] updating load_lc()

--- pyburst/misc/transform.py
+++ pyburst/misc/transform.py
@@ -1,18 +1,28 @@
 import numpy as np
 import os
 
-path = '/home/zac/projects/kepler_grids/misc/'
-filename = 'sim_gs1826_2_xrb9_mean.data'
-filepath = os.path.join(path, filename)
+path = '/home/zac/projects/codes/pyburst/files/temp'
+sim_filename = 'sim_gs1826_2_xrb9_mean.data'
+model_filename = 'gs1826_2_xrb9_mean.data'
 
-modelfile = 'gs1826_2_xrb9_mean.data'
+sim_filepath = os.path.join(path, sim_filename)
+model_filepath = os.path.join(path, model_filename)
 
-def load_lc():
+filepaths = {
+    'model': os.path.join(path, model_filename),
+    'sim': os.path.join(path, sim_filename),
+}
+delims = {'sim': ','}
+
+def load_lc(label):
     """Loads and returns lightcurve data from Duncan
     """
+    filepath = filepaths.get(label)
+
     print(f'Loading {filepath}')
     print('Columns: t, dt, flux, u_flux')
-    table = np.loadtxt(filepath, delimiter=',')
+    table = np.loadtxt(filepath, delimiter=delims.get(label))
+
     return table
 
 
