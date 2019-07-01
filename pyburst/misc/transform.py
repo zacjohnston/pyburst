01105788e6e70df07c60ff67f54ee72dd4588090
Zac <zac.johnston@monash.edu>
2019-07-01 22:31:14 +1000

    [misc/transform.py] added shift_model()

--- pyburst/misc/transform.py
+++ pyburst/misc/transform.py
@@ -1,9 +1,12 @@
 import numpy as np
 import os
 import matplotlib.pyplot as plt
+from astropy import units
 
 # pyburst
 from pyburst.mcmc import burstfit
+from pyburst.misc import anisotropy_tools
+from pyburst.physics import gravity
 
 path = '/home/zac/projects/codes/pyburst/files/temp'
 sim_filename = 'sim_gs1826_2_xrb9_mean.data'
@@ -22,6 +25,7 @@ duncan_params = {
     'd': 6.1,  # kpc
     'inclination': 60,  # deg
     'redshift': 1.26,
+    'mass': 1.76,  # Msun
     't_offset': -10,  # sec
 }
 
@@ -57,20 +61,35 @@ def plot_model():
     return fig, ax
 
 
-def shift_model(model, params):
-    """Shifts given model lightcurve to observer frame
+def shift_model(model, params=duncan_params, source='grid5', version=1):
+    """Returns model lightcurve shifted into observer frame (s, 1e-9 erg/cm^2/s)
     """
-    pass
+    lc = np.zeros_like(model[:, :2])
+    bfit = burstfit.BurstFit(source, version=version)
+    bfit_params = get_burstfit_params(params)
+
+    lc[:, 0] = bfit.shift_to_observer(model[:, 0], bprop='dt', params=bfit_params)
+    lc[:, 0] *= 3600  # bfit outputs in hrs
+    lc[:, 0] -= params['t_offset']
+
+    lc[:, 1] = bfit.shift_to_observer(model[:, 1], bprop='peak', params=bfit_params)
+    lc[:, 1] *= 1e9  # flux units
+
+    return lc
 
 
-def get_burstfit_params(params):
+def get_burstfit_params(params, r_nw=10):
     """Converts Duncan's params to those for input to BurstFit
     """
     out_params = dict()
 
-    out_params['d_b'] = params['d']
-    out_params['m_nw'] = 1.4
-    out_params['m_gr'] = 1.4
-    out_params['xi_ratio'] = 1.0
+    xi_b, xi_p = anisotropy_tools.anisotropy.anisotropy(params['inclination']*units.deg)
+    g = gravity.get_acceleration_newtonian(r=r_nw, m=params['mass'])
+    m_gr, r_gr = gravity.get_mass_radius(g, redshift=params['redshift'])
+
+    out_params['d_b'] = params['d'] * np.sqrt(xi_b)
+    out_params['m_nw'] = params['mass']
+    out_params['m_gr'] = m_gr.value
+    out_params['xi_ratio'] = xi_p / xi_b
 
     return out_params
