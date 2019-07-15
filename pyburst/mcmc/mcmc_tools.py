6f1f313094d55e36911c834169ac906b255c84c1
Zac <zac.johnston@monash.edu>
2019-07-15 13:41:31 +1000

    [mcmc_tools.py] added functions for estimating autocorrelation time

--- pyburst/mcmc/mcmc_tools.py
+++ pyburst/mcmc/mcmc_tools.py
@@ -233,3 +233,51 @@ def check_array(x, n):
     else:
         return np.full(n, x)
 
+
+# ===================================================
+# The following functions adapted from: https://dfm.io/posts/autocorr/
+# ===================================================
+def autocorrelation(y, c=5.0):
+    """Estimates integrated autocorrelation time
+    """
+    f = np.zeros(y.shape[1])
+    for yy in y:
+        f += autocorr_func_1d(yy)
+    f /= len(y)
+    taus = 2.0*np.cumsum(f)-1.0
+    window = auto_window(taus, c)
+    return taus[window]
+
+
+def autocorr_func_1d(x, norm=True):
+    """Returns autocorrelation function for given time series
+    """
+    x = np.atleast_1d(x)
+    if len(x.shape) != 1:
+        raise ValueError("invalid dimensions for 1D autocorrelation function")
+    n = next_pow_two(len(x))
+
+    # Compute the FFT and then (from that) the auto-correlation function
+    f = np.fft.fft(x - np.mean(x), n=2*n)
+    acf = np.fft.ifft(f * np.conjugate(f))[:len(x)].real
+    acf /= 4*n
+
+    if norm:
+        acf /= acf[0]
+    return acf
+
+
+def next_pow_two(n):
+    i = 1
+    while i < n:
+        i = i << 1
+    return i
+
+
+def auto_window(taus, c):
+    m = np.arange(len(taus)) < c * taus
+    if np.any(m):
+        return np.argmin(m)
+    return len(taus) - 1
+
+# ===================================================
