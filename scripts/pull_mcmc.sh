c6baa92cc968d3c1c1066e4ca9658e88c41cbdec
Zac <zac.johnston@monash.edu>
2019-07-16 14:38:20 +1000

    [scripts/pull_mcmc.sh] updated

--- scripts/pull_mcmc.sh
+++ scripts/pull_mcmc.sh
@@ -1,27 +1,23 @@
 #!/bin/bash
-#---------------------------------------------
-#       help
-#---------------------------------------------
+
+
 if [ $# -ne 2 ]; then
   echo "Error: must supply 2 arguments:
 1. source
 2. cluster"
   exit 1
 fi
-#---------------------------------------------
-# relies on ssh alias m=zacpetej@monarch.erc.monash.edu
+
 SOURCE=${1}
 CLUSTER=${2}
 TARGET_MCMC=${KEPLER_GRIDS}/sources/${SOURCE}/mcmc
-TARGET_LOGS=${KEPLER_GRIDS}/sources/${SOURCE}/logs
 
 if [ "${CLUSTER}" == "monarch" ]; then
   PULL_DIR_MCMC="m:/home/zacpetej/id43/kepler_grids/sources/${SOURCE}/mcmc"
-  PULL_DIR_LOGS="m:/home/zacpetej/id43/kepler_grids/sources/${SOURCE}/logs"
 elif [ "${CLUSTER}" == "icer" ]; then
   PULL_DIR_MCMC="isync:/mnt/home/f0003004/kepler_grids/sources/${SOURCE}/mcmc"
-  PULL_DIR_LOGS="isync:/mnt/home/f0003004/kepler_grids/sources/${SOURCE}/logs"
+elif [ "${CLUSTER}" == "c" ]; then
+  PULL_DIR_MCMC="c:/home/zac/projects/kepler_grids/sources/${SOURCE}/mcmc"
 fi
 
 rsync -av --info=progress2 ${PULL_DIR_MCMC}/ ${TARGET_MCMC}/
-rsync -av --info=progress2 ${PULL_DIR_LOGS}/*.o* ${TARGET_LOGS}/
