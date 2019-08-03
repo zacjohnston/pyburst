c0ccb609ff0d885489c41c78309cd812b273c7bb
Zac <zac.johnston@monash.edu>
2019-08-03 19:24:33 +1000

    [mcmc_sample.py] moved plotting order for obs on top in plot()

--- pyburst/mcmc/mcmc_sample.py
+++ pyburst/mcmc/mcmc_sample.py
@@ -203,7 +203,7 @@ class Ksample:
         return np.sum((obs_flux - model_flux)**2 / np.sqrt(obs_flux_err**2 + model_flux_err**2))
 
     def plot(self, residuals=True, shaded=False, alpha_lines=0.3, alpha_shaded=0.7,
-             fontsize=16, xlims=(-10, 200), markersize=1):
+             fontsize=12, xlims=(-10, 200), markersize=1):
         """Plot lightcurve comparison between observed and sample models
         """
         n_subplots = {True: 2, False: 1}.get(residuals)
@@ -226,19 +226,8 @@ class Ksample:
             obs_y_u = np.array(obs_burst.flux_err) / y_scale
 
             # ====== Labelling =====
-            lc_ax[epoch_i].set_ylabel(r'Flux ($10^{-8}$ erg cm$^{-2}$ s$^{-1}$)',
+            lc_ax[epoch_i].set_ylabel(r'$F_\mathrm{b}$ ($10^{-8}$ erg cm$^{-2}$ s$^{-1}$)',
                                       fontsize=fontsize)
-
-            if residuals:
-                res_ax[-1].set_xlabel('Time (s)', fontsize=fontsize)
-                res_ax[epoch_i].errorbar(obs_x, np.zeros_like(obs_x), yerr=obs_y_u,
-                                         ls='none', capsize=3, color='C1',
-                                         markeredgewidth=markersize)
-
-            # ====== Plot observed lightcurves ======
-            lc_ax[epoch_i].errorbar(obs_x, obs_y, yerr=obs_y_u, ls='none',
-                                    capsize=3, color='C1', markersize=markersize,
-                                    markeredgewidth=markersize)
 
             for run_i, run in enumerate(self.runs):
                 model = self.shifted_lc[batch][run]
@@ -268,12 +257,22 @@ class Ksample:
                                        (obs_x-t_shift)) / y_scale
 
                     res_ax[epoch_i].plot(obs_x, y_residuals, color='black',
-                                         alpha=alpha_lines)
+                                         alpha=alpha_lines, zorder=0)
 
                     if shaded:
                         res_ax[epoch_i].fill_between(obs_x, y_residuals - y_residuals_err,
                                                      y_residuals + y_residuals_err,
                                                      color='0.7', alpha=alpha_shaded)
+
+            # ====== Plot observed lightcurves ======
+            lc_ax[epoch_i].errorbar(obs_x, obs_y, yerr=obs_y_u, ls='none',
+                                    capsize=3, color='C1', markersize=markersize,
+                                    markeredgewidth=markersize)
+            if residuals:
+                res_ax[-1].set_xlabel('Time (s)', fontsize=fontsize)
+                res_ax[epoch_i].errorbar(obs_x, np.zeros_like(obs_x), yerr=obs_y_u,
+                                         ls='none', capsize=3, color='C1',
+                                         markeredgewidth=markersize, zorder=10)
 
         lc_ax[-1].set_xlabel('Time (s)', fontsize=fontsize)
         lc_ax[-1].set_xlim(xlims)
