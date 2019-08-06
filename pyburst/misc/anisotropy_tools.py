0a4e7117417fc9a6674c7d1625c7a17ccc71a523
Zac <zac.johnston@monash.edu>
2019-08-06 23:01:44 +1000

    [anisotropy_tools.py] added plot_xi()

--- pyburst/misc/anisotropy_tools.py
+++ pyburst/misc/anisotropy_tools.py
@@ -20,6 +20,33 @@ def load_models(models=None):
     return tables
 
 
+def plot_xi(model='a', fontsize=18):
+    """Plot xi_b, xi_p, and xi_p/xi_b
+    """
+    table = load_models(model)[model]
+
+    fig, ax = plt.subplots()
+    ax.set_xlabel('$i$ (deg)', fontsize=fontsize)
+    ax.set_ylabel(r'$\xi$', fontsize=fontsize)
+    ax.set_xlim([0, 90])
+    ax.set_ylim([-0.0, 4])
+
+    inc = table['col1']
+    inv_xi_b = table['col2'] + table['col3']
+    inv_xi_p = table['col4']
+    xi_ratio = inv_xi_b / inv_xi_p
+
+    ax.plot(inc, 1/inv_xi_b, label=r'$\xi_\mathrm{b}$', linewidth=2)
+    ax.plot(inc, 1/inv_xi_p, ls='--', label=r'$\xi_\mathrm{p}$',
+            linewidth=2)
+    ax.plot(inc, xi_ratio, ls=':',
+            label=r'$\xi_\mathrm{p} / \xi_\mathrm{b}$', linewidth=2)
+
+    ax.legend(fontsize=fontsize)
+    plt.tight_layout()
+    plt.show(block=False)
+
+
 def plot_ratio(tables=None, models=None, fontsize=18):
     """Plot xi_p/xi_b ratio for different models"""
     if tables is None:
