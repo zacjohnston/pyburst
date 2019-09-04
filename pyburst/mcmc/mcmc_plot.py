746bc2c9b12e1f1fa58572a3429fe5edd3d0fce6
Zac Johnston <zac.johnston@monash.edu>
2019-09-04 19:37:17 -0400

    [mcmc_plot.py] added fontsize to plot_distance_anisotropy()

--- pyburst/mcmc/mcmc_plot.py
+++ pyburst/mcmc/mcmc_plot.py
@@ -236,7 +236,7 @@ def plot_gravitational_contours(chain, discard, source, version, cap=None, displ
 
 def plot_inclination(chain, discard, source, version, cap=None, display=True,
                      save=False, disc_model='he16_a', sigmas=np.linspace(0, 2, 5),
-                     summary=False, unit_labels=True, figsize=(4, 4), fontsize=12):
+                     summary=False, unit_labels=True, figsize=(4, 4), fontsize=18):
     """Plots contours of parameters derived using disc model
     """
     disc_chain = mcmc_params.get_disc_chain(chain=chain, discard=discard, cap=cap,
@@ -255,7 +255,7 @@ def plot_inclination(chain, discard, source, version, cap=None, display=True,
 
 def plot_distance_anisotropy(chain, discard, source, version, cap=None, display=True,
                              save=False, sigmas=np.linspace(0, 2, 5), summary=False,
-                             figsize=(4, 4), unit_labels=True):
+                             figsize=(4, 4), unit_labels=True, fontsize=18):
     """Plots contours of MCMC parameters d_b, xi_ratio
     """
     d_b_chain = mcmc_params.get_param_chain(chain, param='d_b', discard=discard,
@@ -266,7 +266,7 @@ def plot_distance_anisotropy(chain, discard, source, version, cap=None, display=
     flat_chain = np.column_stack([d_b_chain, xi_ratio_chain])
     cc = mcmc_tools.setup_custom_chainconsumer(flat_chain, parameters=['d_b', 'xi_ratio'],
                                                sigmas=sigmas, summary=summary,
-                                               unit_labels=unit_labels)
+                                               unit_labels=unit_labels, fontsize=fontsize)
 
     fig = cc.plotter.plot(figsize=figsize)
     fig.subplots_adjust(left=0.2, bottom=0.2)
