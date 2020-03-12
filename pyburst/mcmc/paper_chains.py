2e1ffab673116446a8e2e292dbb753f445f48428
Zac Johnston <zac.johnston@monash.edu>
2020-03-12 14:15:43 -0400

    [paper_chains.py] added get_flat_masses()

--- pyburst/mcmc/paper_chains.py
+++ pyburst/mcmc/paper_chains.py
@@ -1,5 +1,7 @@
 from . import mcmc_tools, mcmc_params
 
+source = 'grid5'
+
 
 def load_chain(version):
     """Load chain used in paper
@@ -7,23 +9,27 @@ def load_chain(version):
     n_steps = {6: 20000}.get(version, 10000)
     compressed = {6: True}.get(version, False)
 
-    return mcmc_tools.load_chain(source='grid5', version=version, n_steps=n_steps,
+    return mcmc_tools.load_chain(source=source, version=version, n_steps=n_steps,
                                  n_walkers=1000, compressed=compressed)
 
 
-def get_radius(chain, version):
+def get_radius(flat_masses):
     """Get flattened Radius chain from full chain
     """
-    flat = {}
-    for key in ['m_nw', 'm_gr']:
-        flat[key] = mcmc_params.get_param_chain(chain, param=key, discard=0,
-                                                source='grid5', version=version)
+    return mcmc_params.get_radius(mass_nw=flat_masses['m_nw'], mass_gr=flat_masses['m_gr'])
 
-    return mcmc_params.get_radius(mass_nw=flat['m_nw'], mass_gr=flat['m_gr'])
+
+def get_flat_masses(chain, version):
+    flat_masses = {}
+    for key in ['m_nw', 'm_gr']:
+        flat_masses[key] = mcmc_params.get_param_chain(chain, param=key, discard=0,
+                                                       source=source, version=version)
+    return flat_masses
 
 
 def get_gravity(chain, version):
     """Get flattened surface gravity (g, 10^14 cm/s^2) from full chain
     """
-    return mcmc_params.get_gravity_chain(chain, discard=0, source='grid5', version=version)
+    return mcmc_params.get_gravity_chain(chain, discard=0, source=source, version=version)
+
 
