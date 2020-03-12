185bbaf7f98fca0606b2401346a59b2233ccc367
Zac Johnston <zac.johnston@monash.edu>
2020-03-12 14:05:03 -0400

    [paper_chains.py] added get_radius()

--- pyburst/mcmc/paper_chains.py
+++ pyburst/mcmc/paper_chains.py
@@ -1,4 +1,4 @@
-from . import mcmc_tools
+from . import mcmc_tools, mcmc_params
 
 
 def load_chain(version):
@@ -9,3 +9,15 @@ def load_chain(version):
 
     return mcmc_tools.load_chain(source='grid5', version=version, n_steps=n_steps,
                                  n_walkers=1000, compressed=compressed)
+
+
+def get_radius(chain, version):
+    """Get flattened Radius chain from full chain
+    """
+    flat = {}
+    for key in ['m_nw', 'm_gr']:
+        flat[key] = mcmc_params.get_param_chain(chain, param=key, discard=0,
+                                                source='grid5', version=version)
+
+    return mcmc_params.get_radius(mass_nw=flat['m_nw'], mass_gr=flat['m_gr'])
+
